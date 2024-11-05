e694831db408c1c820372c240457d6faea79a7ea
Vladyslav Tarasenko <vladislavstargate@gmail.com>
2024-11-05 12:08:14 +0200

    Update juju_connect_mysql script

--- juju_connect_mysql.sh
+++ juju_connect_mysql.sh
@@ -3,8 +3,13 @@
 set -e
 
 # V2
-function get_unit_password {
-    echo $(juju run $1 get-password 2>/dev/null | grep -o "password: [0-9a-zA-Z]\+" | cut -d' ' -f2)
+function get_unit_ip {
+    echo $(juju show-unit $1 2>/dev/null | grep -o " \(public-\)\?address: [0-9.]\+" | cut -d' ' -f3)
 }
 
-juju ssh $1 mysql -h 127.0.0.1 -uroot -p$(get_unit_password $1)
+# V2
+function get_unit_password {
+    echo $(juju run $1 get-password username=clusteradmin 2>/dev/null | grep -o "password: [0-9a-zA-Z]\+" | cut -d' ' -f2)
+}
+
+mysql -h $(get_unit_ip $1) -uclusteradmin -p$(get_unit_password $1)
